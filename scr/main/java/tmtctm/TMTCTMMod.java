5ac36c6dbc76f16995399ddc42e579a50dcb0989
DecWave <focin01052@gmail.com>
2026-05-09 21:14:21 +0300

    Create TMTCTMMod.java

---
+++ scr/main/java/tmtctm/TMTCTMMod.java
@@ -0,0 +1,11 @@
+package tmtctm;
+
+import mindustry.mod.Mod;
+import arc.util.Log;
+
+public class TMTCTMMod extends Mod {
+    @Override
+    public void loadContent() {
+        Log.info("TMTCTM загружен!");
+    }
+}
